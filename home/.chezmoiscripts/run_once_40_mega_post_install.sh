46bc96bfbff9902c0dfdcd715bd5909d23b477fe
edvein-rin <edveinrin@gmail.com>
2022-03-11 13:11:30 +0200

    Cloud symlinks

---
+++ home/.chezmoiscripts/run_once_40_mega_post_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "INFO Creating cloud symlinks"
+ln -s ~/cloud/.vpn .vpn
+ln -s ~/cloud/.ssh .ssh
+ln -s ~/cloud/pictures pictures
+ln -s ~/cloud/documents documents
+ln -s ~/cloud/university university
